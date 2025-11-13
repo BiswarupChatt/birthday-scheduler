bafbe5b6ccaa6ddf3b461f6ae044129ec85470cd
R Q <chatterjeebiswarup61@gmail.com>
2025-11-13 23:25:28 +0530

    date update

--- src/pages/employee/components/EmployeeDialog.jsx
+++ src/pages/employee/components/EmployeeDialog.jsx
@@ -10,7 +10,13 @@ import {
     Grid,
     Switch,
     FormControlLabel,
+    Box
 } from "@mui/material";
+
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import dayjs from "dayjs";
 
 const defaultForm = {
     empId: "",
@@ -30,11 +36,19 @@ export default function EmployeeDialog({
     onSubmit,
     loading = false,
 }) {
-    const [form, setForm] = useState(initialValues);
+    const [form, setForm] = useState(defaultForm);
     const isEdit = mode === "edit";
 
+    // ⭐ NEW: modal date picker state
+    const [openDatePicker, setOpenDatePicker] = useState(false);
+
     useEffect(() => {
-        setForm(initialValues || defaultForm);
+        const safeValues =
+            initialValues && Object.keys(initialValues).length > 0
+                ? initialValues
+                : defaultForm;
+
+        setForm(safeValues);
     }, [initialValues, open]);
 
     const handleChange = (e) => {
@@ -59,84 +73,119 @@ export default function EmployeeDialog({
             <DialogTitle>{isEdit ? "Edit Employee" : "Add Employee"}</DialogTitle>
 
             <DialogContent dividers>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            label="Emp ID"
-                            name="empId"
-                            value={form?.empId}
-                            onChange={handleChange}
-                            fullWidth
-                            required
-                            size="small"
-                        />
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <Grid container spacing={2}>
+
+                        <Grid size={{ xs: 12, md: 6 }}>
+                            <TextField
+                                label="Emp ID"
+                                name="empId"
+                                value={form?.empId}
+                                onChange={handleChange}
+                                fullWidth
+                                required
+                                size="small"
+                            />
+                        </Grid>
+
+                        <Grid size={{ xs: 12, md: 6 }}>
+                            <TextField
+                                label="Designation"
+                                name="designation"
+                                value={form?.designation}
+                                onChange={handleChange}
+                                fullWidth
+                                size="small"
+                            />
+                        </Grid>
+
+                        <Grid size={{ xs: 12, md: 6 }}>
+                            <TextField
+                                label="First Name"
+                                name="firstName"
+                                value={form?.firstName}
+                                onChange={handleChange}
+                                fullWidth
+                                required
+                                size="small"
+                            />
+                        </Grid>
+
+                        <Grid size={{ xs: 12, md: 6 }}>
+                            <TextField
+                                label="Last Name"
+                                name="lastName"
+                                value={form?.lastName}
+                                onChange={handleChange}
+                                fullWidth
+                                size="small"
+                            />
+                        </Grid>
+
+                        <Grid size={{ xs: 12, md: 6 }}>
+                            <TextField
+                                label="Phone"
+                                name="phoneNumber"
+                                value={form?.phoneNumber}
+                                onChange={handleChange}
+                                fullWidth
+                                size="small"
+                            />
+                        </Grid>
+
+                        {/* ⭐ NEW: TextField that opens modal date picker */}
+                        <Grid size={{ xs: 12, md: 6 }}>
+                            <TextField
+                                label="Date of Birth"
+                                value={form?.dateOfBirth || ""}
+                                onClick={() => setOpenDatePicker(true)}
+                                fullWidth
+                                size="small"
+                                placeholder="Select date"
+                                InputProps={{ readOnly: true }}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Box display="flex" justifyContent="flex-end">
+                                <FormControlLabel
+                                    control={
+                                        <Switch
+                                            checked={!!form?.isActive}
+                                            onChange={handleToggle}
+                                        />
+                                    }
+                                    label="Active"
+                                />
+                            </Box>
+                        </Grid>
                     </Grid>
 
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            label="Designation"
-                            name="designation"
-                            value={form?.designation}
-                            onChange={handleChange}
-                            fullWidth
-                            size="small"
-                        />
-                    </Grid>
-
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            label="First Name"
-                            name="firstName"
-                            value={form?.firstName}
-                            onChange={handleChange}
-                            fullWidth
-                            required
-                            size="small"
-                        />
-                    </Grid>
-
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            label="Last Name"
-                            name="lastName"
-                            value={form?.lastName}
-                            onChange={handleChange}
-                            fullWidth
-                            size="small"
-                        />
-                    </Grid>
-
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            label="Phone"
-                            name="phoneNumber"
-                            value={form?.phoneNumber}
-                            onChange={handleChange}
-                            fullWidth
-                            size="small"
-                        />
-                    </Grid>
-
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            label="Date of Birth"
-                            name="dateOfBirth"
-                            type="date"
-                            value={form?.dateOfBirth ? String(form.dateOfBirth).substring(0, 10) : ""}
-                            onChange={handleChange}
-                            fullWidth
-                            size="small"
-                            InputLabelProps={{ shrink: true }}
-                        />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <FormControlLabel
-                            control={<Switch checked={!!form?.isActive} onChange={handleToggle} />}
-                            label="Active"
-                        />
-                    </Grid>
-                </Grid>
+                    {/* ⭐ CENTERED DATE PICKER MODAL */}
+                    <Dialog
+                        open={openDatePicker}
+                        onClose={() => setOpenDatePicker(false)}
+                    >
+                        <DialogContent>
+                            <DateCalendar
+                                value={
+                                    form?.dateOfBirth
+                                        ? dayjs(form.dateOfBirth)
+                                        : null
+                                }
+                                onChange={(value) => {
+                                    setForm((prev) => ({
+                                        ...prev,
+                                        dateOfBirth: value
+                                            ? value.format("YYYY-MM-DD")
+                                            : "",
+                                    }));
+                                    setOpenDatePicker(false); // close modal after select
+                                }}
+                            />
+                        </DialogContent>
+                    </Dialog>
+                </LocalizationProvider>
             </DialogContent>
 
             <DialogActions>
